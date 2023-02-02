751d7bfdd6df109fc3a084ad6dd275d062ecf78a
Sharon Nderi <nderisharonw@gmail.com>
2023-02-02 12:01:45 +0300

    Correct return type for filter_datum

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -63,9 +63,13 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     return mydb
 
 
-def filter_datum(fields: List, redaction: str, message: str,
-                 separator: str) -> List:
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """A function that returns the log message obfuscated.
+    Args: fields (list): list of strings indicating fields to obfuscate
+          redaction (str): what the field will be obfuscated to
+          message (str): the log line to obfuscate
+          separator (str): the character separating the fields
     """
     for field in fields:
         message = re.sub(field+'=.*?'+separator,
