ed43678f8a98bf87de3767ca79ed25dd9d5cdc2d
Sharon Nderi <nderisharonw@gmail.com>
2023-02-17 08:07:23 +0300

    Improve require_auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,13 +12,19 @@ class Auth:
         """Path checker."""
         if path is None:
             return True
-        elif path[-1] != "/":
-            path += "/"
-        if excluded_paths is None or excluded_paths == []:
+        elif excluded_paths is None or excluded_paths == []:
             return True
-        for exclude in excluded_paths:
-            if exclude[:-2] in path:
-                return False
+        elif path in excluded_paths:
+            return False
+        else:
+            for i in excluded_paths:
+                if i.startswith(path):
+                    return False
+                if path.startswith(i):
+                    return False
+                if i[-1] == "*":
+                    if path.startswith(i[:-1]):
+                        return False
         return True
 
     def authorization_header(self, request=None) -> str:
