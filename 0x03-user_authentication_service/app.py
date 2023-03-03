b8ca807fb9f919dd6174aa352d15d2c3fa638102
Sharon Nderi <nderisharonw@gmail.com>
2023-03-03 12:40:42 +0300

    Edit logout

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """A basic flask app for user authentication."""
 
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 from db import DB
 
